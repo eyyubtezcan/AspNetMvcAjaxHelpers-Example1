ca7398ac2e579b9e0534712b2521da795a1604a7
TEZCAN <eyyub.tezcan@gmail.com>
2018-10-28 10:07:50 +0300

    Ajax Helper Methods
    Ajax ActionLink Method Example 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,14 +11,26 @@ namespace AspNetMvcAjaxHelpers_Example1.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            List<string> veriler = new List<string>() { "İstanbul", "Balıkesir", "İzmir", "Muğla", "Antalya" };
+            Session["listem"] = veriler;
+
             return View();
         }
         public PartialViewResult LoadData()
         {
-            List<string> veriler = new List<string>() { "İstanbul", "Balıkesir", "İzmir", "Muğla", "Antalya" };
-
-
+            List<string> veriler = Session["listem"] as List<string>;
+            System.Threading.Thread.Sleep(3000);
             return PartialView("_VeriListesiPartialView", veriler);
+
+        }
+
+        public MvcHtmlString RemoveData(int id)
+        {
+            List<string> veriler = Session["listem"] as List<string>;
+            veriler.RemoveAt(id);
+            Session["Listem"] = veriler;
+            System.Threading.Thread.Sleep(3000);
+            return MvcHtmlString.Empty;
 
         }
     }
